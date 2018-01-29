d22585fdfdcc3ddd908026776d8229dbcc5f7681
Jorge Hortelano <jhortelano@biit-solutions.com>
2018-01-29 17:11:20 +0100

    Added extra log

--- liferay-autoconfigurator-core/src/main/java/com/biit/liferay/auto/factories/Factory.java
+++ liferay-autoconfigurator-core/src/main/java/com/biit/liferay/auto/factories/Factory.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import com.biit.liferay.auto.configuration.AutoConfigurationReader;
+import com.biit.liferay.auto.log.LiferayAutoconfiguratorLogger;
 import com.biit.utils.file.FileReader;
 
 public abstract class Factory<Type> {
@@ -11,8 +12,10 @@ public abstract class Factory<Type> {
 	protected List<File> getDefinitions() {
 		File folder = new File(AutoConfigurationReader.getInstance().getContentFolder() + File.separator + getResourceFolder());
 		if (folder.exists()) {
+			LiferayAutoconfiguratorLogger.info(this.getClass().getName(), "Accessing to files in '" + folder.getAbsolutePath() + "'.");
 			return FileReader.getFiles(folder);
 		}
+		LiferayAutoconfiguratorLogger.debug(this.getClass().getName(), "Accessing to files in project resources.");
 		return FileReader.getResources(getResourceFolder());
 	}
 
